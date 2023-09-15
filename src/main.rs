7628ace2680b783e114348b6ce3fa8881584006e
Luuk van der Duim <luukvanderduim@gmail.com>
2023-09-15 19:30:02 +0200

    Fix unreadable properties.
    Sometimes a property could not be read.
    Having default values in place makes us more resiliant.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use atspi::{
     connection::AccessibilityConnection,
     events::{AddAccessibleEvent, CacheEvents, LegacyAddAccessibleEvent, RemoveAccessibleEvent},
     proxy::{accessible::AccessibleProxy, application::ApplicationProxy},
-    CacheItem, Event,
+    CacheItem, Event, Role,
 };
 use tokio_stream::StreamExt;
 use zbus::{self, MessageType};
@@ -53,34 +53,44 @@ async fn main() -> Result<()> {
                             println!("Root object of Cache event bus_name: {bus_name}, obj_path: {obj_path}");
 
                             // Application Proxy for `app`:
-                            let application_proxy =
+                            if let Ok(application_proxy) =
                                 zbus::ProxyBuilder::<ApplicationProxy>::new(conn)
                                     .interface(APPLICATION_INTERFACE)?
                                     .path(obj_path.as_str())?
                                     .destination(bus_name.as_str())?
                                     .build()
                                     .await
-                                    .expect("could not build ApplicationProxy");
                             {
-                                let toolkit_name: String = application_proxy.toolkit_name().await?;
+                                let toolkit_name = application_proxy
+                                    .toolkit_name()
+                                    .await
+                                    .unwrap_or("Could not read toolkit property".to_string());
                                 println!("toolkit: {toolkit_name}");
                             };
 
                             // AccessibleProxy for `app`:
-                            let accessible_proxy = zbus::ProxyBuilder::<AccessibleProxy>::new(conn)
-                                .interface(ACCESSIBLE_INTERFACE)?
-                                .path(obj_path.as_str())?
-                                .destination(bus_name.as_str())?
-                                .build()
-                                .await
-                                .expect("could not build AccessibleProxy");
+                            if let Ok(accessible_proxy) =
+                                zbus::ProxyBuilder::<AccessibleProxy>::new(conn)
+                                    .interface(ACCESSIBLE_INTERFACE)?
+                                    .path(obj_path.as_str())?
+                                    .destination(bus_name.as_str())?
+                                    .build()
+                                    .await
                             {
-                                let name: String = accessible_proxy.name().await?;
+                                let name: String = accessible_proxy
+                                    .name()
+                                    .await
+                                    .unwrap_or("Could not read name property".to_string());
                                 println!("name: {name}");
-                                let description: String = accessible_proxy.description().await?;
-                                println!("description: {description}");
-                                let role = accessible_proxy.get_role().await?;
 
+                                let description: String = accessible_proxy
+                                    .description()
+                                    .await
+                                    .unwrap_or("Could not obtain a description.".to_string());
+                                println!("description: {description}");
+
+                                let role =
+                                    accessible_proxy.get_role().await.unwrap_or(Role::Unknown);
                                 println!("role: {role}");
                             };
 
